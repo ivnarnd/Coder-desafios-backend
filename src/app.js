d5ca2bd98e1cfec4e992d15bbfad23b52ae42158
Ivan <iaranda264@gmail.com>
2023-09-01 19:04:25 -0300

    configurando el uso de los archivos estaticos

--- src/app.js
+++ src/app.js
@@ -2,14 +2,17 @@ import  Express from "express";
 
 import prodsRouter from "../src/routes/products.routes.js";
 import cardsRouter from "./routes/carts.routes.js";
+import {__dirname} from "./path.js";
+import path from "path";
 
 const PORT = 8080;
 let app = Express();
-
+console.log(__dirname);
 app.use(Express.json());
 app.use(Express.urlencoded({extended:true}));
 
 app.use('/api/products',prodsRouter);
+app.use('/static',Express.static(path.join(__dirname,'/public')))
 app.use('/api/carts',cardsRouter);
 
 app.listen(PORT,()=> console.log(`Servidor corriendo en localhost:${PORT}`));
