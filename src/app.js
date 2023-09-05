47b51000402afcd20834f868065443bbeb3012eb
Ivan <iaranda264@gmail.com>
2023-09-04 21:56:56 -0300

    configurando handlebars

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import prodsRouter from "../src/routes/products.routes.js";
 import cardsRouter from "./routes/carts.routes.js";
 import {__dirname} from "./path.js";
 import path from "path";
+import { engine } from "express-handlebars";
+
 const PORT = 8080;
 let app = Express();
 const storage = multer.diskStorage({
@@ -14,12 +16,17 @@ const storage = multer.diskStorage({
         cb(null,`${Date.now()}${file.originalname}`);
     }
 })
+app.engine('handlebars',engine());
+app.set('view engine','handlebars');
+app.set('views',path.resolve(__dirname,'./views'));
+
 const upload = multer({storage:storage});
 app.use(Express.json());
 app.use(Express.urlencoded({extended:true}));
 app.use('/static',Express.static(path.join(__dirname,'/public')))
 
 app.use('/api/products',prodsRouter);
+app.get('/static',(req,res)=>{res.render('home')});
 app.use('/api/carts',cardsRouter);
 app.post('/upload',upload.single('product'),(req,resp)=>{
     console.log(req.file);
