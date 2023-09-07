d0773cbcf23bbe2e58ad6be04f65b9b0b0844e16
Ivan <iaranda264@gmail.com>
2023-09-07 19:03:20 -0300

    configurando socket io en servidor

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import multer from "multer";
 import prodsRouter from "../src/routes/products.routes.js";
 import cardsRouter from "./routes/carts.routes.js";
 import {__dirname} from "./path.js";
+import { Server } from "socket.io";
 import path from "path";
 import { engine } from "express-handlebars";
 
@@ -15,7 +16,8 @@ const storage = multer.diskStorage({
     filename:(req,file,cb)=>{
         cb(null,`${Date.now()}${file.originalname}`);
     }
-})
+});
+const server = app.listen(PORT,()=> console.log(`Servidor corriendo en localhost:${PORT}`));
 app.engine('handlebars',engine());
 app.set('view engine','handlebars');
 app.set('views',path.resolve(__dirname,'./views'));
@@ -25,6 +27,10 @@ app.use(Express.json());
 app.use(Express.urlencoded({extended:true}));
 app.use('/static',Express.static(path.join(__dirname,'/public')))
 
+const io = new Server(server);
+
+io.on('connection',()=>{console.log('Servidor socket io conectado: ')})
+
 app.use('/api/products',prodsRouter);
 app.get('/static',(req,res)=>{res.render('home')});
 app.use('/api/carts',cardsRouter);
@@ -33,4 +39,4 @@ app.post('/upload',upload.single('product'),(req,resp)=>{
     console.log(req.body);
     resp.status(200).send('Imagen subida');
 })
-app.listen(PORT,()=> console.log(`Servidor corriendo en localhost:${PORT}`));
+
